6a1b4bb05ba6484a2242e752d0dcd3971673d894
jessie <jessie129j@gmail.com>
2022-03-26 21:40:27 +0900

    read message without opening app

--- KakaoTalktoSpeech/app/src/main/java/com/example/kakaotalktospeech/MainActivity.kt
+++ KakaoTalktoSpeech/app/src/main/java/com/example/kakaotalktospeech/MainActivity.kt
@@ -5,34 +5,49 @@ import android.content.Context
 import android.content.Intent
 import android.content.IntentFilter
 import android.os.Bundle
+import android.speech.tts.TextToSpeech
 import android.util.Log
 import android.widget.Switch
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.app.NotificationManagerCompat
+import java.util.*
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), TextToSpeech.OnInitListener {
 
     var switch:Switch?=null
-    var switchOn=false
-    var TTS_Module:TTS_Module?=null
-
     var receiver: BroadcastReceiver ?= null
 
-    override fun onCreate(savedInstanceState: Bundle?) {
+    private var TTS: TextToSpeech? = null
+    private var text:String?=null
+    private var locale: Locale = Locale.KOREAN
+    private var pitch: Float =1.0.toFloat()
+    private var speed: Float =1.0.toFloat()
 
+    override fun onCreate(savedInstanceState: Bundle?) {
         Log.d("myTEST", "onCreate")
+
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        checkNotificationAccess()
-
         switch=findViewById<Switch>(R.id.tbResultItemCount)
-        Log.d("myTEST", "switch is $switch.isChecked()")
 
-        initiateReceiver()
-        initiateTTS()
-
+        initReceiver()
+        checkNotificationAccess()
     }
+
+    fun initReceiver(){
+        receiver = object : BroadcastReceiver() {
+            override fun onReceive(context: Context, intent: Intent) {
+                if (intent!=null&& switch?.isChecked()==true) {
+                    readNotification(intent)
+                }
+            }
+        }
+        val filter = IntentFilter()
+        filter.addAction("com.broadcast.notification")
+        registerReceiver(receiver, filter)
+    }
+
 
     fun checkNotificationAccess() {
         val sets = NotificationManagerCompat.getEnabledListenerPackages(this)
@@ -45,30 +60,29 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
-
-    fun initiateReceiver(){
-        receiver = object : BroadcastReceiver() {
-            override fun onReceive(context: Context, intent: Intent) {
-                if (intent!=null&& switchOn) {
-                    readNotification(intent)
-                }
-            }
-        }
-        val filter = IntentFilter()
-        filter.addAction("com.broadcast.notification")
-        registerReceiver(receiver, filter)
-    }
-
-    fun initiateTTS(){
-        TTS_Module=TTS_Module(this)
-    }
-
     fun readNotification(intent: Intent) {
         Log.d("myTEST", "readNotification")
         val sender=intent.getStringExtra("sender")
         val message=intent.getStringExtra("message")
-        Toast.makeText(this,"message from $sender\n$message", Toast.LENGTH_SHORT).show()
-        TTS_Module?.toSpeech(sender+"님으로부터 메시지가 도착했습니다. "+message)
+        text=sender+"님으로부터 메시지가 도착했습니다. "+message
+        speech()
+    }
+
+    fun speech(){
+        TTS=TextToSpeech(applicationContext,this)
+        TTS!!.setPitch(pitch)
+        TTS!!.setSpeechRate(speed)
+        TTS!!.setLanguage(locale)
+        Toast.makeText(this,text, Toast.LENGTH_SHORT).show()
+    }
+
+    override fun onInit(status: Int) {
+        if (status == TextToSpeech.SUCCESS) {
+            Log.d("myTEST", "onInit - Success")
+            TTS?.speak(text, TextToSpeech.QUEUE_FLUSH, null)
+        } else {
+            Log.d("myTEST", "onInit - Failed")
+        }
     }
 
     override fun onStart(){
@@ -92,11 +106,10 @@ class MainActivity : AppCompatActivity() {
 
     override fun onDestroy() {
         Log.d("myTEST", "onDestroy")
-        TTS_Module!!.destroy()
+        if (TTS != null) {
+            TTS!!.stop();
+            TTS!!.shutdown();
+        }
         super.onDestroy()
     }
-
-
-
-
 }
