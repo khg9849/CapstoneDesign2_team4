a7938d3a110fce72dc05eb3fab14c6535f567bc2
jessie <jessie129j@gmail.com>
2022-03-29 23:24:21 +0900

    show loading activity in the beginning

--- KakaoTalktoSpeech/app/src/main/java/com/example/kakaotalktospeech/LoadingActivity.kt
+++ KakaoTalktoSpeech/app/src/main/java/com/example/kakaotalktospeech/LoadingActivity.kt
@@ -1,11 +1,25 @@
 package com.example.kakaotalktospeech
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
+import android.os.Handler
+import androidx.appcompat.app.AppCompatActivity
+
 
 class LoadingActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_loadingpage)
+
+        val handler = Handler()
+        handler.postDelayed(Runnable {
+            val intent = Intent(applicationContext, MainActivity::class.java)
+            startActivity(intent)
+            finish()
+        }, 1000)
+    }
+    override fun onPause() {
+        super.onPause()
+        finish()
     }
 }
