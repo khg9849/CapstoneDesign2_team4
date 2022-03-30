ddeee5cd2e7c21bb5ef8ac258b69bdd5a221b4ee
lacoon2874 <33238887+lacoon2874@users.noreply.github.com>
2022-03-30 23:06:56 +0900

    add hsj's code

--- hsj/KakaoTalktoSpeech/app/src/main/java/com/example/kakaotalktospeech/LoadingActivity.kt
+++ hsj/KakaoTalktoSpeech/app/src/main/java/com/example/kakaotalktospeech/LoadingActivity.kt
@@ -2,10 +2,11 @@ package com.example.kakaotalktospeech
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.service.notification.NotificationListenerService
 
-class UsefulActivity : AppCompatActivity() {
+class LoadingActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_usefulfeatures)
+        setContentView(R.layout.activity_loadingpage)
     }
 }
